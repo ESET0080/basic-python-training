0959242d023210b0d7bdc97903817f4ee09d8c31
ESET0080 <atharv.ranjan@esyasoft.com>
2025-09-04 21:18:41 +0530

    Basic technical training - python

---
+++ module_demo.py
@@ -0,0 +1,9 @@
+import calculator 
+import math
+from math import sqrt
+import math as m 
+print(calculator.add(5,70))
+print(calculator.sub(570,70))
+print(calculator.multiply(5,70))
+print(calculator.divide(560,70))
+
